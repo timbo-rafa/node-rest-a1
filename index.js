03e169a48a7ac8fe1572575ed72614ccf85b1ffe
Rafael Timbó <timbo.rafa@gmail.com>
2017-10-01 02:12:16 -0400

    v1.8 morgan logger

--- index.js
+++ index.js
@@ -1,20 +1,22 @@
 const SERVER_NAME = 'product-api'
 const PORT = 8000;
 const HOST = '127.0.0.1';
-const utils = require('./utils/utils')
-var restify = require('restify')
+//const utils = require('./utils/utils')
+const restify = require('restify')
+const morganLogger = require('morgan')
 
 // Create the restify server
-var server = restify.createServer({ name: SERVER_NAME})
+const server = restify.createServer({ name: SERVER_NAME})
 
 server
+  .use(morganLogger(':date[web] - :method :url :status :response-time ms'))
   // Allow the use of POST
   .use(restify.fullResponse())
 
   // Maps req.body to req.params so there is no switching between them
   .use(restify.bodyParser())
 
-server.use( utils.requestLogger )
+//server.use( utils.requestLogger )
 
 // Announce endpoints
 console.log('Server endpoints:')
